11851416999c132d40899dd5c4c3224c8102ed12
Mac Mini 15 <macmini15@MCM-OS-115.local>
2022-04-19 11:51:37 -0700

    learningObjects WIP

---
+++ main.js
@@ -0,0 +1,56 @@
+
+// let learningToCode = {
+//     languages: ["JavaScript", "C#", "Java"],
+//     isDifficult: true,
+//     difficulty: 8,
+//     isRewarding: true,
+//     humor: "https://www.reddit.com/r/ProgrammerHumor/"
+// }
+
+// learningToCode.languages.push("Go");
+// learningToCode.difficulty = 7;
+// delete learningToCode.humor;
+// learningToCode.isFun = true;
+
+// for (let i = 0; i < learningToCode.languages.length; i++) {
+//     console.log(learningToCode.languages[i]);
+// }
+
+// for(let key in learningToCode){
+//     console.log(key);
+// }
+
+// for(let key in learningToCode){
+//     console.log(learningToCode[key]);
+// }
+
+//console.log(Object.keys(learningToCode));
+
+//console.log(learningToCode)
+//--------------------------------------------
+
+// Bake a Cake
+
+// let recipe = {
+//     title: 'Chocolate Cake',
+//     serves: 8,
+//     ingredients: ['flour','egg','chocolate']
+// }
+
+// console.log(recipe.title);
+// console.log("Serves: ",recipe.serves);
+// console.log('Ingredients:');
+
+// for (let i = 0; i < recipe.ingredients.length; i++) {
+//     console.log(recipe.ingredients[i]);
+// }
+
+//------------------------------------
+
+// Restaurant to visit
+
+let restaurants = {
+    'Miguels': 'San Diego',  
+};
+
+console.log(restaurants)
